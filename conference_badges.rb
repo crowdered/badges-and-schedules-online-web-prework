0fd1b47631eaadf722dea17744c622838de85a7c
Mark Crowder <crowdered@gmail.com>
2019-03-18 23:11:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,12 +6,12 @@ end
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
-    "Hello, my name is #{attendee}."
+    badge_maker(attendee)
 end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
+  attendees.map.with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 end
